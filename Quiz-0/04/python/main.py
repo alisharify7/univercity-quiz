331e9c36a58c30480a1d777ad1fd109cebc73c49
ali sharifi <87280202+alisharify7@users.noreply.github.com>
2023-03-14 12:02:22 +0330

    Update main.py

--- Quiz-0/04/python/main.py
+++ Quiz-0/04/python/main.py
@@ -1,22 +1,11 @@
 import sys
 
-
-def factorial(n):
-    """
-        this function get number and return factorial of it
-    """
-    if n == 1:
-        return 1
-    return n * factorial(n - 1)
-
-
 def main():
     n = input("Enter a Number: ")
     try:
         n = int(n)
     except ValueError:
         sys.exit("Input Must be Integer")
-
 
     res = 1
     for i in range(1,n+1):
@@ -26,7 +15,6 @@ def main():
 
         if res == n:
             sys.exit(f"{i} != {res}")
-
 
 
 if __name__ == '__main__':
