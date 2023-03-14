1e51ba1b2a115a111d235578a36ac9500764ce37
ali sharifi <87280202+alisharify7@users.noreply.github.com>
2023-03-14 11:59:38 +0330

    Add files via upload

---
+++ Quiz-0/04/C#/app.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter N: ");
+            string input = Console.ReadLine();
+            int n = int.Parse(input);
+
+            int res = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                res *= i;
+                if (res == n)
+                {
+                    Console.WriteLine(string.Format("{0:d}! = {1:d}", i, res));
+                    break;
+                }
+                else if (res > n)
+                {
+                    Console.WriteLine("Not Found");
+                    break;
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
